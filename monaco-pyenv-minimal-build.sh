9a8f41ce67c05ea00e6a0d51eedb73ebf93a2a8f
Simon Biggs <me@simonbiggs.net>
2020-05-27 22:35:19 +1000

    add cheat sheet build script

---
+++ monaco-pyenv-minimal-build.sh
@@ -0,0 +1,7 @@
+# yarn global add uglify-js less
+
+export PYODIDE_PACKAGES="micropip,pycodestyle,python-jsonrpc-server,python-language-server,jedi,autopep8,parso,pyflakes,ujson"
+export HOSTPYTHONROOT=$HOME/.pyenv/versions/3.7.7
+export PKG_CONFIG_PATH=$HOME/.pyenv/versions/3.7.7/lib/pkgconfig
+
+make
